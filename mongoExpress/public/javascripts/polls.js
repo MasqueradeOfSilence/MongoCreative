27e6dbbf97d5e4bea1f3cb0a07bd89df392fe645
Andrew Neville <neville_andrew@yahoo.com>
2017-11-16 19:13:47 -0700

    hard coded array

---
+++ mongoExpress/public/javascripts/polls.js
@@ -0,0 +1,12 @@
+angular.module('poll', [])
+.controller('MainCtrl', [
+  '$scope',
+  function($scope){
+    //$scope.test = 'Hello world!';
+
+    $scope.polls = [
+      {question:'Question with two choices?', c1:'yes', v1:5, c2:'no', v2:10, c3:'', v3:0, c4:'', v4:0, c5:'', v5:0},
+      {question:'Question with five choices?', c1:'yes', v1:0, c2:'no', v2:0, c3:'maybe', v3:0, c4:'???', v4:0, c5:'eh', v5:0}
+    ];
+  }
+]);
