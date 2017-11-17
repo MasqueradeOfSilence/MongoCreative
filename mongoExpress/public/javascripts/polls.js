bd63701cfac61105427a3a4b476e976c825785ce
Andrew Neville <neville_andrew@yahoo.com>
2017-11-16 21:10:23 -0700

    remove defaults

--- mongoExpress/public/javascripts/polls.js
+++ mongoExpress/public/javascripts/polls.js
@@ -4,10 +4,7 @@ angular.module('poll', [])
   function($scope,$http){
     //$scope.test = 'Hello world!';
 
-    $scope.polls = [
-      {question:'Question with two choices?', c1:'yes', v1:5, c2:'no', v2:10, c3:'', v3:0, c4:'', v4:0, c5:'', v5:0},
-      {question:'Question with five choices?', c1:'yes', v1:0, c2:'no', v2:0, c3:'maybe', v3:0, c4:'???', v4:0, c5:'eh', v5:0}
-    ];
+    $scope.polls = [];
 
     $scope.getAll = function() {
       return $http.get('/polls').success(function(data){
