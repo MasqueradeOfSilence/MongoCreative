37264992eaee7abdbb7e402734281ea2dd557336
Andrew Neville <neville_andrew@yahoo.com>
2017-11-16 22:00:57 -0700

    don't use create()

--- mongoExpress/public/javascripts/polls.js
+++ mongoExpress/public/javascripts/polls.js
@@ -27,7 +27,8 @@ angular.module('poll', [])
       console.log("adding poll");
       console.log($scope.question);
       var newobject = {question:$scope.question, c1:'yes', v1:5, c2:'no', v2:10, c3:'', v3:0, c4:'', v4:0, c5:'', v5:0};
-      $scope.create(newobject);
+      //$scope.create(newobject);
+      $scope.polls.push(newobject);
       $scope.question = '';
     };
 
