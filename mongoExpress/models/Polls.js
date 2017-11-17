85399d821375e65fcb9cca915b82d098437316ee
Andrew Neville <neville_andrew@yahoo.com>
2017-11-16 22:27:14 -0700

    fixed type

--- mongoExpress/models/Polls.js
+++ mongoExpress/models/Polls.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var PollSchema = new mongoose.Schema({
   question: String,
-  c1: {type: Number, default: 0},
+  c1: String,
   v1: {type: Number, default: 0},
-  c2: {type: Number, default: 0},
+  c2: String,
   v2: {type: Number, default: 0},
-  c3: {type: Number, default: 0},
+  c3: String,
   v3: {type: Number, default: 0},
-  c4: {type: Number, default: 0},
+  c4: String,
   v4: {type: Number, default: 0},
-  c5: {type: Number, default: 0},
+  c5: String,
   v5: {type: Number, default: 0},
 });
 
